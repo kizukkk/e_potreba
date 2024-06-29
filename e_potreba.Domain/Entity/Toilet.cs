189244f344e3a6a36e647710b578d029102a57ea
kizukkk <zenro934@gmail.com>
2024-06-30 01:03:57 +0300

    added base domain entities

---
+++ e_potreba.Domain/Entity/Toilet.cs
@@ -0,0 +1,13 @@
+﻿using e_potreba.Domain.Entity.Common;
+
+namespace e_potreba.Domain.Entity;
+public class Toilet : BaseEntity
+{
+    public required string Address;
+    public required string City;
+    public required string Country;
+    public required User Author;
+    public required Point Point;
+    public required string Status;
+    public required int Rating;
+}
