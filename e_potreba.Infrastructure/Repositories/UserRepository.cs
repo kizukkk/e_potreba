9537977f165ecc3f5e8f40cef8d5a403f93b279f
kizukkk <zenro934@gmail.com>
2024-06-30 01:05:57 +0300

    Added empty implementation of repo interfaces

---
+++ e_potreba.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,7 @@
+﻿using e_potreba.Application.Repositories;
+using e_potreba.Domain.Entity;
+
+namespace e_potreba.Infrastructure.Repositories;
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+}
