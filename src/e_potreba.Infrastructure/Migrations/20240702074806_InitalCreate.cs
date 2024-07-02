774658643fa2cb93277f20e9cfe07a24ea243f97
kizukkk <zenro934@gmail.com>
2024-07-02 12:05:41 +0300

    init base migration

---
+++ src/e_potreba.Infrastructure/Migrations/20240702074806_InitalCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_potreba.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Toilets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTimeCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateTimeModify = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Toilets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTimeCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateTimeModify = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Toilets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
