0e5b79e2c928ec2535a8ac0c57f0f49685173eae
kizukkk <zenro934@gmail.com>
2024-07-02 12:04:09 +0300

    added dbContext class

---
+++ src/e_potreba.Infrastructure/DatabaseContext/MsSqlDatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using e_potreba.Domain.Entity;
+
+namespace e_potreba.Infrastructure.DatabaseContext;
+public class MsSqlDatabaseContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public MsSqlDatabaseContext(
+        DbContextOptions<MsSqlDatabaseContext> options, 
+        IConfiguration configuration
+        )
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Toilet> Toilets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
