27293b6a432606591c34ea7c06acba5d8bf32bfa
kizukkk <zenro934@gmail.com>
2024-07-02 10:58:05 +0300

    created db config class

---
+++ src/e_potreba.Infrastructure/ServiceExtensions/DatabaseExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using e_potreba.Infrastructure.DatabaseContext;
+
+namespace e_potreba.Infrastructure.ServiceExtensions;
+public static class DatabaseExtension
+{
+    public static void DatabaseConfigure(
+        this IServiceCollection services, 
+        IConfiguration configuration
+        )
+    {
+        var targetDatabase = configuration.GetSection("TargetDatabase").Value;
+        var connectionString = configuration.GetConnectionString(targetDatabase);
+
+        services.AddDbContext<MsSqlDatabaseContext>(opt => opt.UseSqlServer(connectionString));
+    }
+}
