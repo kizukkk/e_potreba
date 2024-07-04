6d307e0b3c7f5d4feb448d053b367ad11b5486b4
kizukkk <zenro934@gmail.com>
2024-07-04 10:34:14 +0300

    created auth user reponse records

---
+++ src/e_potreba.Application/DTO/Auth/AuthUserResponse.cs
@@ -0,0 +1,12 @@
+﻿using e_potreba.Domain.Enum;
+
+namespace e_potreba.Application.DTO.Auth;
+public sealed record AuthUserResponse(
+    Guid id, 
+    string login, 
+    string email, 
+    UserRole role,
+    string token
+    )
+{
+}
