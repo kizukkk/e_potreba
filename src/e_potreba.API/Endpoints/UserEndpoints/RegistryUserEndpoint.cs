8853eda16d173258707537c617796c954bb5ea1e
kizukkk <zenro934@gmail.com>
2024-07-04 10:33:42 +0300

    created base of user registry endpoint

---
+++ src/e_potreba.API/Endpoints/UserEndpoints/RegistryUserEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.ApiEndpoints;
+using e_potreba.Application.DTO.Auth;
+using e_potreba.Application.DTO.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_potreba.API.Endpoints.UserEndpoints;
+
+[Route("api/v1/users/registry")]
+public class RegistryUserEndpoint : EndpointBaseSync
+    .WithRequest<UserRequest>
+    .WithActionResult<AuthUserResponse>
+{
+    [HttpPost]
+    public override ActionResult<AuthUserResponse> Handle(
+        [FromBody] UserRequest request
+        )
+    {
+        return Ok();
+    }
+}
