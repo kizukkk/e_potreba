ef8266879bda46fd837ab198bda874691e7538dd
kizukkk <zenro934@gmail.com>
2024-07-13 00:11:36 +0300

    relocated db migration in another class

--- src/e_potreba.API/Program.cs
+++ src/e_potreba.API/Program.cs
@@ -1,17 +1,13 @@
-﻿using e_potreba.Infrastructure.DatabaseContext;
 using e_potreba.Infrastructure.ServiceExtensions;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.DatabaseConfigure(builder.Configuration);
+builder.Services.DatabaseConfig(builder.Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-var serviceScope = app.Services.CreateScope();
-var dataContext = serviceScope.ServiceProvider.GetService<MsSqlDatabaseContext>();
-dataContext?.Database.Migrate();
+DatabaseExtension.DatabaseMigrate(app.Services.CreateScope()); 
 
 
 app.MapControllers();
