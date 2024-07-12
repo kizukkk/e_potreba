290b55a1339dc53c94ebe97eafd6a6528a4ba4dd
kizukkk <zenro934@gmail.com>
2024-07-13 00:10:03 +0300

    Added method for migration db with using Polly retry

--- src/e_potreba.Infrastructure/ServiceExtensions/DatabaseExtension.cs
+++ src/e_potreba.Infrastructure/ServiceExtensions/DatabaseExtension.cs
@@ -2,12 +2,15 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using e_potreba.Infrastructure.DatabaseContext;
+using Microsoft.Data.SqlClient;
+using Polly.Retry;
+using Polly;
 
 namespace e_potreba.Infrastructure.ServiceExtensions;
 public static class DatabaseExtension
 {
-    public static void DatabaseConfigure(
-        this IServiceCollection services, 
+    public static void DatabaseConfig(
+        this IServiceCollection services,
         IConfiguration configuration
         )
     {
@@ -16,4 +19,31 @@ public static class DatabaseExtension
 
         services.AddDbContext<MsSqlDatabaseContext>(opt => opt.UseSqlServer(connectionString));
     }
+
+    public static void DatabaseMigrate(
+        this IServiceScope serviceScope
+        )
+    {
+        ResiliencePipeline pipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions
+            {
+                ShouldHandle = args => args.Outcome switch
+                {
+                    {Exception: SqlException } => PredicateResult.True(),
+                    _ => PredicateResult.False(),
+                },
+                MaxRetryAttempts = 2,
+                OnRetry = retryArgumets =>
+                {
+                    Console.WriteLine($"Current attemp: {retryArgumets.AttemptNumber}, {retryArgumets.Outcome.Exception}");
+                    return ValueTask.CompletedTask;
+                }
+            })
+            .AddTimeout(TimeSpan.FromSeconds(3))
+            .Build();
+
+        var dataContext = serviceScope.ServiceProvider.GetService<MsSqlDatabaseContext>();
+        pipeline.Execute(() => dataContext?.Database.Migrate());
+    }
+
 }
