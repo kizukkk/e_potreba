9c70a6e35e89425d115b39674f5ed0c4a223c576
kizukkk <zenro934@gmail.com>
2024-07-13 00:12:24 +0300

    added user reg services scratch

---
+++ src/e_potreba.Application/Service/AuthServices/UserRegistryService.cs
@@ -0,0 +1,19 @@
+﻿using e_potreba.Application.Abstractions;
+using e_potreba.Application.Repositories;
+
+namespace e_potreba.Application.Service.AuthServices;
+public class UserRegistryService
+{
+    private readonly IUserRepository _userRepo;
+    private readonly IJwtProvider _jwtProvider;
+
+
+    public UserRegistryService(IUserRepository userRepo, IJwtProvider jwtProvider)
+    {
+        _userRepo = userRepo;
+        _jwtProvider = jwtProvider;
+    }
+
+
+    
+}
