52a7959ced469d22b41af5109b6360311ba385c8
kizukkk <zenro934@gmail.com>
2024-07-14 18:54:01 +0300

    added resilnce pipeline and temp error handler

--- src/e_potreba.API/Program.cs
+++ src/e_potreba.API/Program.cs
@@ -1,4 +1,9 @@
+﻿using e_potreba.Application.Repositories;
+using e_potreba.Application.Service.AuthServices;
+using e_potreba.Infrastructure.Repositories;
 using e_potreba.Infrastructure.ServiceExtensions;
+using e_potreba.Infrastructure.Policy;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +12,29 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddSingleton<UserRegistryService>();
+
+builder.Services.AddResiliencePipeline("default-pipeline", builder =>   
+{
+    builder.AddRetry(ResiliencePolicyOptions.RetryStrategy);
+    builder.AddTimeout(ResiliencePolicyOptions.TimeoutStrategy);
+    builder.Build();
+});
 var app = builder.Build();
 
 DatabaseExtension.DatabaseMigrate(app.Services.CreateScope()); 
 
+//Temp error handler
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next(context);
+    }
+    catch
+    {
+        context.Response.StatusCode = 500;
+    }
+});
 
 app.MapControllers();
 
