6fed0841e755c6c5a16805814931739cfe5d7aa1
kizukkk <zenro934@gmail.com>
2024-07-14 18:47:08 +0300

    added static class of resilince strategy options

---
+++ src/e_potreba.Infrastructure/Policy/ResiliencePolicyOptions.cs
@@ -0,0 +1,39 @@
+﻿namespace e_potreba.Infrastructure.Policy;
+
+using Microsoft.Data.SqlClient;
+using Polly.Timeout;
+using Polly.Retry;
+using Polly;
+public static class ResiliencePolicyOptions
+{
+
+    public static readonly TimeoutStrategyOptions TimeoutStrategy = 
+        new TimeoutStrategyOptions
+        {
+            Timeout = TimeSpan.FromSeconds(5),
+            OnTimeout = static args =>
+            {
+                Console.WriteLine($"{args.Context.OperationKey}: Execution timed out after {args.Timeout.TotalSeconds} seconds.");
+                return default;
+            }
+        };
+
+    public static readonly RetryStrategyOptions RetryStrategy =
+        new RetryStrategyOptions
+        {
+            ShouldHandle = args => args.Outcome switch
+            {
+                { Exception: SqlException } => PredicateResult.True(),
+                { Exception: TaskCanceledException } => PredicateResult.True(),
+                _ => PredicateResult.True(),
+            },
+            MaxRetryAttempts = 2,
+            OnRetry = retryArgumets =>
+            {
+                Console.WriteLine($"Current attemp: {retryArgumets.AttemptNumber}, " +
+                    $"{retryArgumets.Outcome.Exception}");
+                
+                return ValueTask.CompletedTask;
+            }
+        };
+}
