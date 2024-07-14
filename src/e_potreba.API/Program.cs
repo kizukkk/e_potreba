5f65d3b3a4356a173b80203f1988f61033fcaa6c
kizukkk <zenro934@gmail.com>
2024-07-14 18:51:46 +0300

    added registry user repo and user registr service into startup

--- src/e_potreba.API/Program.cs
+++ src/e_potreba.API/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.DatabaseConfig(builder.Configuration);
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<UserRegistryService>();
 var app = builder.Build();
 
 DatabaseExtension.DatabaseMigrate(app.Services.CreateScope()); 
