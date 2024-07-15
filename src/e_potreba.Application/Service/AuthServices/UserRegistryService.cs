8076e515d5945fe72ae8c5b20b8badb93a5e6e58
kizukkk <zenro934@gmail.com>
2024-07-15 11:22:37 +0300

    added resilience pipeline and execute method

--- src/e_potreba.Application/Service/AuthServices/UserRegistryService.cs
+++ src/e_potreba.Application/Service/AuthServices/UserRegistryService.cs
@@ -1,17 +1,32 @@
 ﻿using e_potreba.Application.Abstractions;
 using e_potreba.Application.Repositories;
 
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
 namespace e_potreba.Application.Service.AuthServices;
 public class UserRegistryService
 {
     private readonly IUserRepository _userRepo;
     private readonly IJwtProvider _jwtProvider;
+    private readonly ResiliencePipeline _resiliencePipeline;
 
 
-    public UserRegistryService(IUserRepository userRepo, IJwtProvider jwtProvider)
+    public UserRegistryService(
+        IUserRepository userRepo, 
+        IJwtProvider jwtProvider,
+        [FromKeyedServices("default-pipeline")]
+        ResiliencePipeline pipeline
+        )
     {
         _userRepo = userRepo;
         _jwtProvider = jwtProvider;
+        _resiliencePipeline = pipeline;
+    }
+
+    public async Task Execute(CancellationToken token)
+    {
+        throw new NotImplementedException();
     }
 
 
