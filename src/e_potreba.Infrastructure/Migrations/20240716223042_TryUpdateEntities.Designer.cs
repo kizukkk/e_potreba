2318e821b562ba2383c0829908989067165dc505
kizukkk <zenro934@gmail.com>
2024-07-17 01:35:36 +0300

    added update entities migration

--- src/e_potreba.Infrastructure/Migrations/20240716223042_TryUpdateEntities.Designer.cs
+++ src/e_potreba.Infrastructure/Migrations/20240716223042_TryUpdateEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e_potreba.Infrastructure.DatabaseContext;
 
@@ -11,9 +12,11 @@ using e_potreba.Infrastructure.DatabaseContext;
 namespace e_potreba.Infrastructure.Migrations
 {
     [DbContext(typeof(MsSqlDatabaseContext))]
-    partial class MsSqlDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240716223042_TryUpdateEntities")]
+    partial class TryUpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
